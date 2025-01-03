73615f2ca7247836c68ea246c6a44e43c2f31cfb
TIAO-Eliasse <eliassetiao@gmail.com>
2025-01-03 13:23:40 +0100

    Update Accueil.py

--- Application_MES/Accueil.py
+++ Application_MES/Accueil.py
@@ -1,8 +1,14 @@
 import streamlit as st
 import os
+from PIL import Image
+def data_dir(data_name):
+    main=os.path.dirname(__file__)
+    return os.path.join(main,data_name)
+imag_path=data_dir("OIP.jpg")
 st.write("###                                     Theme")
 st.write("##### Mecanisme de transmissions des flucutuations des prix de pétroles dans l'économie de la Guinée Equatoriale")
-st.image("OIP.jpg", caption="Guinée Equatoriale", use_column_width=True)
+#st.image(Image.open(imag_path), caption="Guinée Equatoriale", use_column_width=True)
+
 st.markdown("Cette application a été réalisé par:")
 
 st.write(" TIAO Eliasse")
@@ -18,8 +24,6 @@ st.markdown("""
     </style>
     """, unsafe_allow_html=True)
 
-def data_dir(data_name):
-    main=os.path.dirname(__file__)
-    return os.path.join(main,data_name)
+
 if st.button("Continuez"):
     st.switch_page("pages/1_AnalyseExploratoire.py")
